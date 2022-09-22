e1f38ebfd48f3a1bcb62b7fc81bc56bb696743a2
EduHerr <eduherrald@gmail.com>
2022-09-22 01:52:31 -0500

    Vistas Svelte implementadas. Falta paginado

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,7 @@
 <main>
 	<div class="container w-75 mt-5 rounded shadow bg-white">
 		<div class="row align-items-stretch">
+			<h4 class="text-center">Weedz Collection</h4>
 			<div class="col table-responsive">
 				<table class="table table-sm table-striped table-bordered text-center">
 					<thead class="bg-primary text-white">
@@ -13,16 +14,31 @@
 							<th>Sustancia</th>
 						</tr>
 					</thead>
-					<tbody></tbody>
+					<tbody>
+						{#each _Weedz as Weed}
+							<tr>
+								<td>{Weed.nombre}</td>
+								<td>{Weed.calificacion}</td>
+								<td>{Weed.tipo}</td>
+								<td>{Weed.efecto}</td>
+								<td>{Weed.aroma}</td>
+								<td>{Weed.sustancia}</td>
+							</tr>
+						{/each}
+					</tbody>
 				</table>
 			</div>
 		</div>
 	</div>
 </main>
-
-<style>
-</style>
-
 <script>
-	
+	import axios from "axios";
+	import { onMount } from "svelte";
+
+	let _Weedz = [];
+
+	onMount(async() => {
+		const { data } = await axios.get("/api/leer");
+		_Weedz = data.message;
+	});
 </script>
